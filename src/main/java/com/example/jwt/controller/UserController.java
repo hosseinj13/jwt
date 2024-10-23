7b096be8042f73cfc23e77003f58bba362b2a26c
hossein javadi <hosseinj13@yahoo.com>
2024-10-23 06:54:39 +0330

    welcome to jwt

---
+++ src/main/java/com/example/jwt/controller/UserController.java
@@ -0,0 +1,67 @@
+package com.example.jwt.controller;
+
+import com.example.jwt.dto.AuthRequest;
+import com.example.jwt.model.UserInfo;
+import com.example.jwt.service.JwtService;
+import com.example.jwt.service.UserInfoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class UserController {
+
+    private UserInfoService service;
+
+    private JwtService jwtService;
+
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    public UserController(UserInfoService service, JwtService jwtService, AuthenticationManager authenticationManager) {
+        this.service = service;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome this endpoint is not secure";
+    }
+
+    @PostMapping("/addNewUser")
+    public String addNewUser(@RequestBody UserInfo userInfo) {
+        return service.addUser(userInfo);
+    }
+
+    @GetMapping("/user/userProfile")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public String userProfile() {
+        return "Welcome to User Profile";
+    }
+
+    @GetMapping("/admin/adminProfile")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public String adminProfile() {
+        return "Welcome to Admin Profile";
+    }
+
+    @PostMapping("/generateToken")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+        );
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getUsername());
+        } else {
+            throw new UsernameNotFoundException("Invalid user request!");
+        }
+    }
+}
---
+++ src/main/java/com/example/jwt/service/UserInfoService.java
@@ -0,0 +1,46 @@
+package com.example.jwt.service;
+
+import com.example.jwt.model.UserInfo;
+import com.example.jwt.model.UserInfoDetails;
+import com.example.jwt.repository.UserInfoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+@RequiredArgsConstructor
+
+public class UserInfoService implements UserDetailsService {
+
+    private UserInfoRepository repository;
+
+    private PasswordEncoder encoder;
+
+    @Autowired
+    public UserInfoService(UserInfoRepository repository, PasswordEncoder encoder) {
+        this.repository = repository;
+        this.encoder = encoder;
+    }
+
+    @Override
+    public UserInfoDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<UserInfo> userDetail = repository.findByEmail(username); // Assuming 'email' is used as username
+
+        // Converting UserInfo to UserDetails
+        return userDetail.map(UserInfoDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+    }
+
+    public String addUser(UserInfo userInfo) {
+        // Encode password before saving the user
+        userInfo.setPassword(encoder.encode(userInfo.getPassword()));
+        repository.save(userInfo);
+        return "User Added Successfully";
+    }
+}
